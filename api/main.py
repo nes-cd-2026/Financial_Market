5900d22db38b738321cb7d50738f85b9b09c2d2e
Pietrobds <pietrobds651@gmail.com>
2026-06-13 08:41:22 -0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -13,22 +13,56 @@ def health():
     return {"status": "ok"}
 
 @app.get("/dados")
-def dados(ativo: str):
+def dados(
+
+    ativo: str,
+
+    inicio: str = None,
+
+    fim: str = None
+
+):
 
     query = f"""
-    SELECT a.name,
-           p.date,
-           p.price
+    SELECT
+        a.name,
+        p.date,
+        p.price
     FROM prices p
     JOIN assets a
-      ON p.asset_id = a.id
-    WHERE a.name = '{ativo}'
-    ORDER BY p.date
+      ON p.asset_id = a.asset_id
+    WHERE a.name='{ativo}'
     """
 
-    df = pd.read_sql(query, engine)
+    if inicio:
 
-    return df.to_dict(orient="records")
+        query += f"""
+        AND p.date >= '{inicio}'
+        """
+
+    if fim:
+
+        query += f"""
+        AND p.date <= '{fim}'
+        """
+
+    query += """
+
+    ORDER BY p.date
+
+    """
+
+    df = pd.read_sql(
+        query,
+        engine
+    )
+
+    return (
+        df
+        .to_dict(
+            orient="records"
+        )
+    )
 
 @app.get("/resumo")
 def resumo(ativo: str):
