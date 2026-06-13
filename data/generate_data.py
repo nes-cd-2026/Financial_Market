37151b75f7c9354e3483279f8ee38bbdf87974ee
Pietrobds <pietrobds651@gmail.com>
2026-06-13 07:31:35 -0300

    modificando generate_data.py para a api

--- data/generate_data.py
+++ data/generate_data.py
@@ -1,88 +1,120 @@
 import pandas as pd
 import numpy as np
-from sqlalchemy import create_engine
-from sqlalchemy import text
+
+from sqlalchemy import (
+    create_engine,
+    text
+)
+
 import time
 
-# esperar o banco iniciar
 time.sleep(10)
 
 engine = create_engine(
     "postgresql://user:password@db:5432/finance"
 )
 
-# criar tabelas
 with engine.connect() as conn:
 
     conn.execute(text("""
-        CREATE TABLE IF NOT EXISTS assets (
-            id SERIAL PRIMARY KEY,
-            name TEXT NOT NULL
-        );
+
+    CREATE TABLE IF NOT EXISTS assets(
+
+        id SERIAL PRIMARY KEY,
+
+        name TEXT
+
+    )
+
     """))
 
     conn.execute(text("""
-        CREATE TABLE IF NOT EXISTS prices (
-            id SERIAL PRIMARY KEY,
-            asset_id INT REFERENCES assets(id),
-            date DATE NOT NULL,
-            price FLOAT NOT NULL
-        );
+
+    CREATE TABLE IF NOT EXISTS prices(
+
+        id SERIAL PRIMARY KEY,
+
+        asset_id INT,
+
+        date DATE,
+
+        price FLOAT
+
+    )
+
     """))
 
     conn.commit()
 
-# remover dados antigos
-with engine.connect() as conn:
-    conn.execute(text("DELETE FROM prices"))
-    conn.execute(text("DELETE FROM assets"))
-    conn.commit()
+assets = pd.DataFrame({
 
-# gerar dados
-np.random.seed(42)
+"name":[
+"AAPL",
+"GOOGL",
+"TSLA"
+]
 
-assets = ["AAPL", "GOOGL", "TSLA"]
-
-assets_df = pd.DataFrame({
-    "name": assets
 })
 
-dates = pd.date_range("2023-01-01", "2023-12-31")
+assets.to_sql(
+"assets",
+engine,
+if_exists="replace",
+index=False
+)
 
-price_data = []
+dates = pd.date_range(
+"2023-01-01",
+"2023-12-31"
+)
 
-for i, asset in enumerate(assets, start=1):
+rows=[]
 
-    price = 100
+np.random.seed(42)
 
-    for date in dates:
+for asset_id in [1,2,3]:
 
-        price *= np.random.normal(1.0005, 0.02)
+    price=100
 
-        price_data.append([
-            i,
-            date,
-            round(price, 2)
+    for d in dates:
+
+        price*=np.random.normal(
+            1.0005,
+            0.02
+        )
+
+        rows.append([
+
+            asset_id,
+            d,
+            round(price,2)
+
         ])
 
-prices_df = pd.DataFrame(
-    price_data,
-    columns=["asset_id", "date", "price"]
+prices=pd.DataFrame(
+
+rows,
+
+columns=[
+
+"asset_id",
+"date",
+"price"
+
+]
+
 )
 
-# inserir dados
-assets_df.to_sql(
-    "assets",
-    engine,
-    if_exists="append",
-    index=False
+prices.to_sql(
+
+"prices",
+
+engine,
+
+if_exists="replace",
+
+index=False
+
 )
 
-prices_df.to_sql(
-    "prices",
-    engine,
-    if_exists="append",
-    index=False
-)
-
-print("Dados gerados com sucesso!")
+print("dados criados")
